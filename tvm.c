89908a4442d98b4d65bdd7079ce9fb35d9890b15
Liam Treanor <Liamandtreanor@gmail.com>
2026-08-29 19:02:40 +1200

    add enums

--- tvm.c
+++ tvm.c
@@ -1,21 +1,68 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+#include <stdarg.h>
 
-typedef char trit;
+enum vmmode {
+    FAST,
+    PACKED
+} vmmode;
 
-typedef struct nonet {
-    trit t0;
-    trit t1;
-    trit t2;
-    trit t3;
-    trit t4;
-    trit t5;
-    trit t6;
-    trit t7;
-    trit t8;
-} nonet;
+typedef enum operation {
+    IDN,    // identity / noop
 
+    SET,    // set register to literal
+    LOAD,   // set register to memory value
+    COPY,   // set register to value in another register
+    STORE,  // store register value in memory
 
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    MOD,
+    NEG,
+    INC,
+    DEC,
+
+    CMP,    // evaluate / compare
+    JU,     // jump unconditional
+    JL,     // jump if less
+    JE,     // jump if equal
+    JG,     // jump if greater
+    JNL,    // jump if not less
+    JNE,    // jump if not equal
+    JNG,    // jump if not greater
+
+    CF,     // call function
+    EF,     // end function / return
+
+    EP      // end program / halt
+} operation;
+
+char mode = FAST;
+
+typedef uint8_t byte;
+typedef uint8_t trit;
+typedef trit u9[9];
+#define U9_MAX 19683 // 3 ^ 3 ^ 2
+u9 memory[U9_MAX] = {0};
+u9 r[27] = {0};
+
+int as_number(u9 nonet) {
+    int result = 0;
+    int power = 1;
+    for (int i = 0; i < 9; i++) {
+        result += nonet[i] * power;
+        power *= 3;
+    }
+    return result;
+}
+
+int op(operation opcode, ...) {
+
+}
 
 void main(int argc, char** argv) {
-    printf("Hello, World!");
+    
 }
